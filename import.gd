d4b1daef1772f68647fe3f3ae3e09d20b364d437
vichug <vic.hvk@gmail.com>
2015-06-22 01:52:54 +0200

    added an option for importing only the tiles from the Tiled tilesets which are used in the map, and not the whole tilesets. Can reduce significantly the size of the Godot scene file and the overall working time with said tilesets. This option shouldn't be used always, because it has the obvious drawback that it will import the TIled tilesets only partially.

--- import.gd
+++ import.gd
@@ -4,17 +4,50 @@ extends EditorPlugin
 
 var map_path = ""
 var error_popup
+var corres_array = []
 
 const FLIPPED_HORIZONTALLY_FLAG = 0x80000000
 const FLIPPED_VERTICALLY_FLAG   = 0x40000000
 const FLIPPED_DIAGONALLY_FLAG   = 0x20000000
 
+var only_used_tiles
+
+func _on_CheckBox_toggled( pressed ): #the checkbox "import only used tiles" is connected with this function through a signal
+	if (pressed):
+		only_used_tiles = true
+		print (only_used_tiles)
+		
+	else:
+		only_used_tiles = false
+		print (only_used_tiles)
+
 func _ready():
+
 	error_popup = get_node("ErrorPopup")
 
-func createTileset(var data, var cell_size):
+func createTileset(var data, var cell_size, var onlyused, var layers): # data = tileset_data = map_data[tilesets]
 	var ts = TileSet.new()
 	var size = cell_size
+	
+	if (onlyused) : #create a list containing the ids of all the Tiled tilesets tiles which are effectively used in the map
+		for l in layers:
+			var i = 0
+			for y in range(0, l["height"]):
+				for x in range(0, l["width"]):
+					#get the gid as a string first, to prevent rounding error
+					var strgid = str(l["data"][i])
+					var gid = int(strgid)
+					if (gid != 0):
+						#read the flags from gid
+						var flipped_horizontally = (gid & FLIPPED_HORIZONTALLY_FLAG)
+						var flipped_vertically = (gid & FLIPPED_VERTICALLY_FLAG)
+						var flipped_diagonally = (gid & FLIPPED_DIAGONALLY_FLAG)
+						#clear the flags to get the actual tile id
+						#print("\n before the rotation flags are cleared it is : ", gid)
+						gid &= ~(FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG | FLIPPED_DIAGONALLY_FLAG)
+
+						corres_array.append(gid)
+					i += 1
 	
 	for t in data:
 		var path = map_path.get_base_dir() + "/" + t["image"]
@@ -39,95 +72,96 @@ func createTileset(var data, var cell_size):
 
 		if t.has("tiles"):
 			tiles = t["tiles"]
-
 		if t.has("tileproperties"):
 			tiles_prop = t["tileproperties"]
 
-		for y in range(0, height, cell_size.y+spacing):
-			for x in range(0, width, cell_size.x+spacing):
-				var xy = Vector2(x, y)
-				var rect = Rect2(xy, size)
-				ts.create_tile(count)
-				ts.tile_set_texture(count, texture)
-				ts.tile_set_region(count, rect)
-				var id = str(count - int(t["firstgid"]))
 				
-				# set nav property on tile for navigation
-				# add entire tile (deal with polygons later)
-				if t.has("tileproperties"):
-					for tilep in tiles_prop:
+		for y in range(0, height, cell_size.y + spacing):
+			for x in range(0, width, cell_size.x + spacing):
+				var intcount = int(count)
+				if ( !onlyused || ( onlyused && ( corres_array.find(intcount) != -1))): #when onlyused is true, the tile from Tiled tileset is imported in Godot tileset only if it is used in the map
+					var xy = Vector2(x, y)
+					var rect = Rect2(xy, size)
+					ts.create_tile(count)
+					ts.tile_set_texture(count, texture)
+					ts.tile_set_region(count, rect)
+					var id = str(count - int(t["firstgid"]))
 
-						if tilep == id and tiles_prop[tilep].has("nav"):
-							var nav_shape = NavigationPolygon.new()
-							var nav_array = Vector2Array()
-								
-							nav_array.push_back(Vector2(0,0))
-							nav_array.push_back(Vector2(cell_size.x,0))
-							nav_array.push_back(Vector2(cell_size.x,cell_size.y))
-							nav_array.push_back(Vector2(0,cell_size.y))
+					# set nav property on tile for navigation
+					# add entire tile (deal with polygons later)
+					if t.has("tileproperties"):
+						for tilep in tiles_prop:
 
-							nav_shape.add_polygon(IntArray([0,1,2,3]))
-							nav_shape.set_vertices(nav_array)
-							nav_shape.set_outline(0, nav_array)
-							ts.tile_set_navigation_polygon(count, nav_shape)
+							if tilep == id and tiles_prop[tilep].has("nav"):
+								var nav_shape = NavigationPolygon.new()
+								var nav_array = Vector2Array()
+									
+								nav_array.push_back(Vector2(0,0))
+								nav_array.push_back(Vector2(cell_size.x,0))
+								nav_array.push_back(Vector2(cell_size.x,cell_size.y))
+								nav_array.push_back(Vector2(0,cell_size.y))
+
+								nav_shape.add_polygon(IntArray([0,1,2,3]))
+								nav_shape.set_vertices(nav_array)
+								nav_shape.set_outline(0, nav_array)
+								ts.tile_set_navigation_polygon(count, nav_shape)
 
 
-				if t.has("tiles"):
-					for tile in tiles:
-						if tile == id and tiles[tile].has("objectgroup"):
-							for obj in tiles[tile]["objectgroup"]["objects"]:
-								if !obj.has("polyline") and !obj.has("polygon") and !obj.has("ellipse"):
-									var w = obj["width"]
-									var h = obj["height"]
-									var xx = obj["x"]
-									var yy = obj["y"]
-									var rectshape = RectangleShape2D.new()
-									rectshape.set_extents(Vector2(w/2, h/2))
-									ts.tile_set_shape(count, rectshape)
-									ts.tile_set_shape_offset(count, Vector2(w/2 + xx, h/2 + yy))
-								elif obj.has("ellipse"):
-									var w = obj["width"]
-									var h = obj["height"]
-									var xx = obj["x"]
-									var yy = obj["y"]
-									if w == h:
-										var circleshape = CircleShape2D.new()
-										circleshape.set_radius(w/2)
-										ts.tile_set_shape(count, circleshape)
+					if t.has("tiles"):
+						for tile in tiles:
+							if tile == id and tiles[tile].has("objectgroup"):
+								for obj in tiles[tile]["objectgroup"]["objects"]:
+									if !obj.has("polyline") and !obj.has("polygon") and !obj.has("ellipse"):
+										var w = obj["width"]
+										var h = obj["height"]
+										var xx = obj["x"]
+										var yy = obj["y"]
+										var rectshape = RectangleShape2D.new()
+										rectshape.set_extents(Vector2(w/2, h/2))
+										ts.tile_set_shape(count, rectshape)
 										ts.tile_set_shape_offset(count, Vector2(w/2 + xx, h/2 + yy))
-									else:
-										var capsuleshape = CapsuleShape2D.new()
-										capsuleshape.set_radius(w/2)
-										capsuleshape.set_height(h/2)
-										ts.tile_set_shape(count, capsuleshape)
-										ts.tile_set_shape_offset(count, Vector2(w/2 + xx, h/2 + yy))
-								elif obj.has("polygon"):
-									var polygonshape = ConvexPolygonShape2D.new()
-									var vectorarray = Vector2Array()
+									elif obj.has("ellipse"):
+										var w = obj["width"]
+										var h = obj["height"]
+										var xx = obj["x"]
+										var yy = obj["y"]
+										if w == h:
+											var circleshape = CircleShape2D.new()
+											circleshape.set_radius(w/2)
+											ts.tile_set_shape(count, circleshape)
+											ts.tile_set_shape_offset(count, Vector2(w/2 + xx, h/2 + yy))
+										else:
+											var capsuleshape = CapsuleShape2D.new()
+											capsuleshape.set_radius(w/2)
+											capsuleshape.set_height(h/2)
+											ts.tile_set_shape(count, capsuleshape)
+											ts.tile_set_shape_offset(count, Vector2(w/2 + xx, h/2 + yy))
+									elif obj.has("polygon"):
+										var polygonshape = ConvexPolygonShape2D.new()
+										var vectorarray = Vector2Array()
 
-									var xx = obj["x"]
-									var yy = obj["y"]
+										var xx = obj["x"]
+										var yy = obj["y"]
 
-									for point in obj["polygon"]:
-										vectorarray.push_back(Vector2(point["x"] + xx, point["y"] + yy))
+										for point in obj["polygon"]:
+											vectorarray.push_back(Vector2(point["x"] + xx, point["y"] + yy))
 
-									polygonshape.set_points(vectorarray)
-									ts.tile_set_shape(count, polygonshape)
-									ts.tile_set_shape_offset(count, Vector2(0, 0))
-								elif obj.has("polyline"):
-									var polygonshape = ConcavePolygonShape2D.new()
-									var vectorarray = Vector2Array()
+										polygonshape.set_points(vectorarray)
+										ts.tile_set_shape(count, polygonshape)
+										ts.tile_set_shape_offset(count, Vector2(0, 0))
+									elif obj.has("polyline"):
+										var polygonshape = ConcavePolygonShape2D.new()
+										var vectorarray = Vector2Array()
 
-									var xx = obj["x"]
-									var yy = obj["y"]
+										var xx = obj["x"]
+										var yy = obj["y"]
 
-									for point in obj["polyline"]:
-										vectorarray.push_back(Vector2(point["x"] + xx, point["y"] + yy))
+										for point in obj["polyline"]:
+											vectorarray.push_back(Vector2(point["x"] + xx, point["y"] + yy))
 
-									polygonshape.set_segments(vectorarray)
-									ts.tile_set_shape(count, polygonshape)
-									ts.tile_set_shape_offset(count, Vector2(0, 0))
-
+										polygonshape.set_segments(vectorarray)
+										ts.tile_set_shape(count, polygonshape)
+										ts.tile_set_shape_offset(count, Vector2(0, 0))
 				count += 1
 
 	return ts
@@ -158,9 +192,11 @@ func _on_Button_pressed():
 	var tilemap_root = Node2D.new()
 	tilemap_root.set_name("Tile Map")
 
+	var layers = map_data["layers"]
+
 	var tileset_data = map_data["tilesets"]
 	var cell_size = Vector2(map_data["tilewidth"], map_data["tileheight"])
-	var tileset = createTileset(tileset_data, cell_size)
+	var tileset = createTileset(tileset_data, cell_size, only_used_tiles, layers)
 
 	if(!tileset):
 		print("Something went wrong while creating the tileset. Make sure all files are at the right path")
@@ -169,7 +205,7 @@ func _on_Button_pressed():
 	root_node.add_child(tilemap_root)
 	tilemap_root.set_owner(root_node)
 
-	var layers = map_data["layers"]
+	
 	for l in layers:
 		var layer_map = TileMap.new()
 		tilemap_root.add_child(layer_map)
@@ -194,9 +230,8 @@ func _on_Button_pressed():
 					var flipped_vertically = (gid & FLIPPED_VERTICALLY_FLAG)
 					var flipped_diagonally = (gid & FLIPPED_DIAGONALLY_FLAG)
 
-					#clear the flags to get the actual tile id
+					#clear the flags to get the actual tile id 
 					gid &= ~(FLIPPED_HORIZONTALLY_FLAG | FLIPPED_VERTICALLY_FLAG | FLIPPED_DIAGONALLY_FLAG)
-
 					layer_map.set_cell(x, y, gid, flipped_horizontally, flipped_vertically)
 				i += 1
 
